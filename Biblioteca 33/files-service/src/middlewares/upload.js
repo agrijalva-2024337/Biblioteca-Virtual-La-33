144384c6ab2608ac337d6db29b285d3efbd09edc
agrijalva-2024337 <agrijalva-2024337@kinal.edu.gt>
2026-03-08 21:08:11 -0600

    feat: Agregar configuración de cloudinary y actualizar File Service

--- Biblioteca 33/files-service/src/middlewares/upload.js
+++ Biblioteca 33/files-service/src/middlewares/upload.js
@@ -1,15 +1,13 @@
 const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("../configs/cloudinary.js");
 
-const storage = multer.diskStorage({
-
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "biblioteca-files",
+    resource_type: "raw"
   }
-
 });
 
 module.exports = multer({ storage });
